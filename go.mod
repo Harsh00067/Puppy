cf35151ce511bc785e52ed630b9f1da152637618
Harsh00067 <126866253+Harsh00067@users.noreply.github.com>
2025-06-24 16:16:33 +0530

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Harsh00067/Puppy
 
 go 1.22.3
+
+require github.com/Harsh00067/dog v0.0.0-20250624102348-99842239dc3d // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/Harsh00067/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	dog.WhenGrownUp(Barks())
 }
